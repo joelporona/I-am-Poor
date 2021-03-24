51447ef191db6a19c4f9968dd87a29d8c2899394
Joel Perez Orona <joelp.orona@gmail.com>
2021-03-24 18:58:30 +0100

    Initial Commit

---
+++ I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Boruga on 2021-03-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
